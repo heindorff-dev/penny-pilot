0dbb634644712081de6006a849e4aa25df8b3d8a
heindorff-dev <andreas.heindorff@proton.me>
2024-10-04 19:45:32 +0200

    docker config

--- api/main.go
+++ api/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
-    "net/http"
+	"net/http"
 
-    "github.com/go-chi/chi/v5"
-    "github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-    r := chi.NewRouter()
-    r.Use(middleware.Logger)
-    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("Hello World!"))
-    })
-    http.ListenAndServe(":3001", r)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Mount("/api", Api())
+	http.ListenAndServe(":3001", r)
+}
+
+func Api() chi.Router {
+	r := chi.NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+		w.WriteHeader(http.StatusOK)
+	})
+	return r
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: true,
+    watch: {
+       usePolling: true,
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
