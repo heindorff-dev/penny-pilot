d3ee0c8d8b9bd39d0eb2a043ae22d79c44ebf011
heindorff-dev <andreas.heindorff@proton.me>
2024-10-05 12:10:00 +0200

    add navigation menu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,31 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/home-page'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
 
 function App() {
 
   return (
     <>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
       <Routes>
         <Route path="/" element={<HomePage/>}/> {/* 👈 Renders at /app/ */}
       </Routes>
