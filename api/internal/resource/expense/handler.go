3f717b201253f8956f2d6b3fc1e9da187773b57f
heindorff-dev <andreas.heindorff@proton.me>
2024-10-12 03:24:28 +0200

    expense handler update

--- api/internal/resource/expense/handler.go
+++ api/internal/resource/expense/handler.go
@@ -2,55 +2,64 @@ package expense
 
 import (
 	"api/internal/database"
-	"api/service"
-	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
+	"github.com/gocql/gocql"
 )
 
 type Expense struct {
-	Amount int       `json:"amount"`
-	Name   string    `json:"name"`
-	Id     uuid.UUID `json:"id"`
+	Amount int        `json:"amount"`
+	Name   string     `json:"name"`
+	Id     gocql.UUID `json:"id"`
 }
 
-func GetExpense(ctx *gin.Context) {
-	id := ctx.Param("id")
-	parsed, err := uuid.Parse(id)
+func GetExpense(c *gin.Context) {
+	var (
+		id     gocql.UUID
+		name   string
+		amount int
+	)
+
+	paramId := c.Param("id")
+	parsedId, err := gocql.ParseUUID(paramId)
 
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
 	}
 
-	expense, err := service.GetExpenseById(parsed)
+	query := "SELECT id, name, amount FROM Expense where id = ? LIMIT 1"
+	err = database.Session.Query(query, parsedId).WithContext(c).Consistency(gocql.One).Scan(&id, &name, &amount)
 
 	if err != nil {
-		ctx.JSON(http.StatusOK, gin.H{"message": err.Error()})
+		c.JSON(http.StatusOK, gin.H{"message": err.Error()})
 		return
 	}
 
-	ctx.JSON(http.StatusOK, expense)
+	expense := Expense{
+		Id:     id,
+		Name:   name,
+		Amount: amount,
+	}
+
+	c.JSON(http.StatusOK, expense)
 }
 
 func GetExpenses(c *gin.Context) {
 	var (
 		queryParamName = c.Query("name")
 		expenses       = []Expense{}
-		id             uuid.UUID
+		id             gocql.UUID
 		name           string
 		amount         int
 		err            error
 	)
 
-	query := "SELECT id, name, amount FROM Expense where name LIKE %" + queryParamName + "%"
+	query := "SELECT id, name, amount FROM Expense where name LIKE '%?%'"
 
-	fmt.Println("query: ", query)
-
-	scanner := database.Session.Query(query).WithContext(c).Iter().Scanner()
+	scanner := database.Session.Query(query, queryParamName).WithContext(c).Iter().Scanner()
 
 	for scanner.Next() {
 		err = scanner.Scan(&id, &name, &amount)
